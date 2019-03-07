13d9ade620a7dbaafe725a905b73bb707805fa52
pasquino <pasquale.merolle@gmail.com>
2019-03-07 23:20:27 +0100

    lesson 14

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="navbar">This is the header</header>
+        )
+    }
+}
+
+export default Header
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainContent extends Component {
+    render() {
+        return (
+            <main>This is the main section</main>
+        )
+    }
+}
+
+export default MainContent
